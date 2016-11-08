74f2bca1516bea50c165254d89ef0c22fff8bbe5
Andy Mortimer <Andy.Mortimer@specialistholidays.com>
2016-11-08 21:05:01 +0000

    Added local jshon so no longer have a requirement to have jshon installed globally, added some validation checks and rename the jira api to host for clarity.

--- README.md
+++ README.md
@@ -5,9 +5,20 @@
 npm install git-old-jira-branches -g
 ```
 
-## Runing the code
-Call git-old-jira-branches passing in your Jira username and password as shown below, you will then be prompted to delete and close branches the code finds.
+## Setup
+You have two methods for setting up the code the preferred way is to save your username, password and [host](#host) to [git config](#git-config-setup), the alternative approach is to pass the [arguments](#cli-arguments) into ```git-old-jira-branches```.
 
+### Host
+The Jira host should be just the base url i.e. **jira.atlassian.com** the rest of the URL is added via the code.
+
+### Git config setup
 ```bash
-git-old-jira-branches -u [username] -p [password]
+git config jira.username [username]
+git config jira.password [passwrd]
+git config jira.host [host]
+```
+
+### CLI Arguments
+```bash
+git-old-jira-branches -u [username] -p [password] -h [jira host url]
 ```
--- deleteClosedBranch.sh
+++ deleteClosedBranch.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# libaries
+JSHON="$(npm bin)/jshon"
+
+# settings
 USERNAME="$(git config jira.username)"
 PASSWORD="$(git config jira.password)"
-API="$(git config jira.api)"
+HOST="$(git config jira.host)"
+
+# type
+BOLD="\e[01m"
+
+# colours
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+CYAN="\e[36m"
+RESET="\e[00m"
+
 
 while getopts ":u:p:a:" opt; do
     case $opt in
@@ -12,60 +27,60 @@ while getopts ":u:p:a:" opt; do
         p)
             # Jira Password
             PASSWORD=$OPTARG ;;
-        a)
+        h)
             # Jira API
-            API=$OPTARG ;;
+            HOST=$OPTARG ;;
     esac
 done
 
-if [ -z $USERNAME ] || [ -z $PASSWORD ] || [ -z $API ]; then
-    printf "\n\e[31mYou must supply a 'Username', 'Password' AND 'API'.\n"
-    printf "You can supply these by setting 'git congig' settings or pass them in as command line arguments.\e[00m\n\n"
+if [ -z $USERNAME ] || [ -z $PASSWORD ] || [ -z $HOST ]; then
+    printf "\n${RED}You must supply a 'Username', 'Password' AND 'HOST'.\n"
+    printf "You can supply these by setting 'git congig' settings or pass them in as command line arguments.${RESET}\n\n"
     printf "To setup git config settings run the following commands.\n\n"
-    printf "\e[32m\tgit config jira.username [username]\n"
-    printf "\tgit config jira.password [password]\n"
-    printf "\tgit config jira.api [api]\n\n"
-    printf "\e[00mOr you can pass these options in as arguments with the following\n"
-    printf "\e[32m\tgit-old-jira-branches -u [username] -p [password] -a [api]\n\e[00m\n"
+    printf "${GREEN}\tgit config jira.username [username] #your jira username\n"
+    printf "\tgit config jira.password [password] #your jira password\n"
+    printf "\tgit config jira.host [host] #this should be the jira url i.e jira.atlassian.net\n\n"
+    printf "${RESET}Or you can pass these options in as arguments with the following\n"
+    printf "${GREEN}\tgit-old-jira-branches -u [username] -p [password] -h [host]\n${RESET}\n"
 
     exit 1;
 fi
 
-
-printf "\e[34m###########################################################################################\e[00m"
-printf "\n\t\e[01mRunning checks on git branches, please be patient this could take some time.\e[00m\n"
-printf "\e[34m###########################################################################################\e[00m\n\n"
+printf "${BLUE}###########################################################################################${RESET}"
+printf "\n\t${BOLD}Running checks on git branches, please be patient this could take some time.${RESET}\n"
+printf "${BLUE}###########################################################################################${RESET}\n\n"
 
 ## need to removed folder from branch
-for BRANCH in $(git branch -a | grep -v "\*" | xargs -n 1); do
+for BRANCH in $(git branch -a --merged master| grep -v "\*" | xargs -n 1); do
     JIRA_BRANCH=$(sed "s/.*\///g" <<< $BRANCH)
 
-    TICKET=$(curl -s -u $USERNAME:$PASSWORD -H "Content-Type: application/json" https://$API/rest/api/2/issue/$JIRA_BRANCH)
+    TICKET=$(curl -s -u $USERNAME:$PASSWORD -H "Content-Type: application/json" https://$HOST/rest/api/2/issue/$JIRA_BRANCH)
 
-    ERROR_MESSAGE=$(jshon -Q -e errorMessages -e 0 -u  <<< $TICKET)
+    if [ "$($JSHON -e errorMessages -t <<< $TICKET 2> /dev/null)" == "array" ]; then
+        ERROR_MESSAGE=$($JSHON -Q -e errorMessages -e 0 -u  <<< $TICKET)
 
-    if [ "$ERROR_MESSAGE" != 'Issue Does Not Exist' ]; then
-        TICKET_STATUS=$(jshon -Q -e fields -e status -e name -u <<< $TICKET)
-        TICKET_SUMMARY=$(jshon -Q -e fields -e summary -u <<< $TICKET)
+        if [ "$ERROR_MESSAGE" != 'Issue Does Not Exist' ]; then
+            TICKET_STATUS=$(jshon -Q -e fields -e status -e name -u <<< $TICKET)
+            TICKET_SUMMARY=$(jshon -Q -e fields -e summary -u <<< $TICKET)
 
-        printf "Found ticket \e[01m$JIRA_BRANCH\e[00m with the following status \e[00;36m[$TICKET_STATUS]\e[00m\n"
+            printf "Found ticket ${BOLD}$JIRA_BRANCH${RESET} with the following status ${BOLD}[$TICKET_STATUS]${RESET}\n"
 
-        if [ "$TICKET_STATUS" == 'Closed' ] || [ "$TICKET_STATUS" == "In Live Environment" ] ; then
-            printf "\n\n\e[34m\e[01mDo you want to delete\e[0m:"
-            printf "\n\e[01m$BRANCH $TICKET_SUMMARY \e[00;36m[$TICKET_STATUS]\e[00m\n"
+            if [ "$TICKET_STATUS" == 'Closed' ] || [ "$TICKET_STATUS" == "In Live Environment" ] ; then
+                printf "\n\n${BLUE}${BOLD}Do you want to delete${RESET}:"
+                printf "\n${BOLD}$BRANCH $TICKET_SUMMARY [$TICKET_STATUS]${RESET}\n"
 
-            echo "The following commands will be run, please make sure you confirm you really want to delete these."
-            printf "\n\e[00;31mgit branch -D '$BRANCH'\e[00m";
-            printf "\n\e[00;31mgit push origin :$BRANCH\e[00m";
-            read -p $'\n\e[34mDelete branch?\e[0m: (y/n) ' CONT
+                echo "The following commands will be run, please make sure you confirm you really want to delete these."
+                printf "\n${RED}git branch -D '$BRANCH'${RESET}";
+                printf "\n${RED}git push origin :$BRANCH${RESET}";
+                read -p $'\n${RED}Delete branch?${RESET}: (y/n) ' CONT
 
-            if [ "$CONT" == "y" ]; then
-              git branch -D $BRANCH > /dev/null 2>&1
-              git push origin :BRANCH > /dev/null 2>&1
-              printf "\e[32mDELETED Branch \e[01m$BRANCH\e[00m\n\n";
+                if [ "$CONT" == "y" ]; then
+                  git branch -D $BRANCH > /dev/null 2>&1
+                  git push origin :BRANCH > /dev/null 2>&1
+                  printf "${GREEN}DELETED Branch \e[01m$BRANCH${RESET}\n\n";
+                fi
             fi
         fi
-
     fi
 done
 
